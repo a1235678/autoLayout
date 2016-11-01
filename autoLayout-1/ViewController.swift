b9ca18931703693f1a48058547c1fc07f3dd2ccd
zeroplus <z1235678@gmail.com>
2016-11-01 17:13:38 +0800

    Initial Commit

---
+++ autoLayout-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoLayout-1
+//
+//  Created by zero plus on 2016/11/1.
+//  Copyright © 2016年 zero plus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ autoLayout-1Tests/autoLayout_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  autoLayout_1Tests.swift
+//  autoLayout-1Tests
+//
+//  Created by zero plus on 2016/11/1.
+//  Copyright © 2016年 zero plus. All rights reserved.
+//
+
+import XCTest
+@testable import autoLayout_1
+
+class autoLayout_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
